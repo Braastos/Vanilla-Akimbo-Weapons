16192b93961ce7a5d0d133f1ef872d36428bc5b5
Braastos <braastos@gmx.de>
2018-02-10 06:04:35 +0100

    Add files via upload
    Fixed that the Akimbo SMG was not Akimbo

---
+++ prototypes/technology.lua
@@ -0,0 +1,143 @@
+data.raw['technology']['military'].prerequisites = {"apistol_tech"}
+data.raw['technology']['military-3'].prerequisites = {"asmg_tech","ashotgun_tech"}
+
+
+data:extend
+(
+	{
+		{
+			type = "technology",
+			name = "apistol_tech",
+			icon = "__akimbo-weapons__/graphics/technology/pistol128.png",
+			icon_size = 128,
+			effects =
+			{
+				{
+					type = "unlock-recipe",
+					recipe = "apistol"
+				},		
+			},
+			unit =
+			{
+				count = 5,
+				ingredients =
+				{
+					{"science-pack-1", 1},
+					},
+				time = 20
+			},
+			order = "c-g-a-b-a",
+		},
+		{
+			type = "technology",
+			name = "asmg_tech",
+			icon = "__akimbo-weapons__/graphics/technology/submachinegun128.png",
+			icon_size = 128,
+			effects =
+			{
+				{
+					type = "unlock-recipe",
+					recipe = "asmg"
+				},		
+			},
+			prerequisites = {"military-2", "apistol_tech"},
+			unit =
+			{
+				count = 25,
+				ingredients =
+				{
+					{"science-pack-1", 5},
+					{"science-pack-2", 5},
+					{"science-pack-3", 5},
+					{"military-science-pack", 10}
+				},
+				time = 20
+			}, 
+			order = "c-g-a-b-b",
+		},
+		{
+			type = "technology",
+			name = "ashotgun_tech",
+			icon = "__akimbo-weapons__/graphics/technology/shotgun128.png",
+			icon_size = 128,
+			effects =
+			{
+				{
+					type = "unlock-recipe",
+					recipe = "ashotgun"
+				},		
+			},
+			prerequisites = {"military-2", "apistol_tech"},
+			unit =
+			{
+				count = 25,
+				ingredients =
+				{
+					{"science-pack-1", 5},
+					{"science-pack-2", 5},
+					{"science-pack-3", 5},
+					{"military-science-pack", 10}
+				},
+				time = 20
+			},
+			order = "c-g-a-b-c",
+		},
+		{
+			type = "technology",
+			name = "acombat-shotgun_tech",
+			icon = "__akimbo-weapons__/graphics/technology/combatshotgun128.png",
+			icon_size = 128,
+			effects =
+			{
+				{
+					type = "unlock-recipe",
+					recipe = "acombat-shotgun"
+				},		
+			},
+			prerequisites = {"military-4", "asmg_tech","ashotgun_tech"},
+			unit =
+			{
+				count = 50,
+				ingredients =
+				{
+					{"science-pack-1", 5},
+					{"science-pack-2", 5},
+					{"science-pack-3", 5},
+					{"military-science-pack", 10}
+				},
+				time = 20
+			}, 
+			order = "c-g-a-b-d",
+		},
+		{
+		type = "technology",
+		name = "minigun_tech",
+		icon = "__akimbo-weapons__/graphics/technology/minigun128.png",
+		icon_size = 128,
+		effects =
+		{
+		  {
+			type = "unlock-recipe",
+			recipe = "minigun"
+		  }
+		},
+		prerequisites = {"military-4","acombat-shotgun_tech","artillery"},
+		unit =
+		{
+		  count = 750,
+		  ingredients =
+		  {
+			{"science-pack-1", 5},
+			{"science-pack-2", 5},
+			{"science-pack-3", 5},
+			{"military-science-pack", 10},
+			{"production-science-pack", 1},
+			{"high-tech-science-pack", 1},
+			{"space-science-pack", 2},			
+		  },
+		  time = 60
+		},
+		order = "c-g-a-b-e"
+	  },
+	}
+)
